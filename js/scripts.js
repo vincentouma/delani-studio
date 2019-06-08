d2e574133f6f05aac5315bc580d7f235e0fe9391
vincentouma <vinceoumah@gmail.com>
2019-06-09 02:47:56 +0300

    Add jQuery to what we do

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+$(".img1").click(function(){
+    $(".img1").toggle(function(){
+$(".para1").show();
+    });
+});
+
+$(".img2").click(function(){
+    $(".img2").toggle(function(){
+        $(".para2").show();
+    });
+});
+
+$(".img3").click(function(){
+    $(".img3").toggle(function(){
+        $(".para3").show();
+    });
+});
+});
