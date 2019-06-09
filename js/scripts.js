01bb6c3d519f54e274f04e2fbe9bcda0c8b35b0c
vincentouma <vinceoumah@gmail.com>
2019-06-09 14:38:36 +0300

    Add all changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,37 +1,49 @@
 $(document).ready(function(){
 $(".img1").click(function(){
     $(".img1").toggle(function(){
-$(".para1").show();
+$(".para1").show(function(){
+    $("#h3a").css('font-weight','bold');
+});
     });
 });
 
 $(".img2").click(function(){
     $(".img2").toggle(function(){
-        $(".para2").show();
+        $(".para2").show(function(){
+            $("#h3b").css('font-weight','bold');
+        });
     });
 });
 
 $(".img3").click(function(){
     $(".img3").toggle(function(){
-        $(".para3").show();
+        $(".para3").show(function(){
+            $("#h3c").css('font-weight','bold');
+        });
     });
 });
 
 $(".para1").click(function(){
     $(".para1").toggle(function(){
-        $(".img1").show();
+        $(".img1").show(function(){
+            $("#h3a").css('font-weight','normal');
+        });
     });
 });
 
 $(".para2").click(function(){
     $(".para2").toggle(function(){
-        $(".img2").show();
+        $(".img2").show(function(){
+            $("#h3b").css('font-weight','normal');
+        });
     });
 });
 
 $(".para3").click(function(){
     $(".para3").toggle(function(){
-        $(".img3").show();
+        $(".img3").show(function(){
+            $("#h3c").css('font-weight','normal');
+        });
     });
 });
 
