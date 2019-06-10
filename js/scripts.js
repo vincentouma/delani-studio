7fc77c7f18ac3b5c5c97ae219dc7d925e1ae05dc
vincentouma <vinceoumah@gmail.com>
2019-06-10 14:26:17 +0300

    all changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,65 +1,63 @@
-$(document).ready(function() {
-  $(".img1").click(function() {
-    $(".img1").toggle(function() {
-      $(".para1", ).show(function() {
+$(document).ready(function () {
+  $(".img1").click(function () {
+    $(".img1").toggle(function () {
+      $(".para1", ).show(function () {
         $("#h3a").css('font-weight', 'bold');
       });
     });
   });
 
-  $(".img2").click(function() {
-    $(".img2").toggle(function() {
-      $(".para2").show(function() {
+  $(".img2").click(function () {
+    $(".img2").toggle(function () {
+      $(".para2").show(function () {
         $("#h3b").css('font-weight', 'bold');
       });
     });
   });
 
-  $(".img3").click(function() {
-    $(".img3").toggle(function() {
-      $(".para3").show(function() {
+  $(".img3").click(function () {
+    $(".img3").toggle(function () {
+      $(".para3").show(function () {
         $("#h3c").css('font-weight', 'bold');
       });
     });
   });
 
-  $(".para1").click(function() {
-    $(".para1").toggle(function() {
-      $(".img1").show(function() {
+  $(".para1").click(function () {
+    $(".para1").toggle(function () {
+      $(".img1").show(function () {
         $("#h3a").css('font-weight', 'normal');
       });
     });
   });
 
-  $(".para2").click(function() {
-    $(".para2").toggle(function() {
-      $(".img2").show(function() {
+  $(".para2").click(function () {
+    $(".para2").toggle(function () {
+      $(".img2").show(function () {
         $("#h3b").css('font-weight', 'normal');
       });
     });
   });
 
-  $(".para3").click(function() {
-    $(".para3").toggle(function() {
-      $(".img3").show(function() {
+  $(".para3").click(function () {
+    $(".para3").toggle(function () {
+      $(".img3").show(function () {
         $("#h3c").css('font-weight', 'normal');
       });
     });
   });
 
-  $("#contact").submit(function(e) {
+  $("#contact").submit(function (e) {
 
     var user = document.getElementById("name").value;
     alert(user + " your massage has been recieved succesfuly and thanks for keeping in touch");
 
   });
 
-  
+
   $(".prjct").hide();
-  $(".pic1").hover(function(){
-      $(this).find(".prjct").toggle(400);
-  });
-  
+  $(".pic1").hover(function () {
+    $(this).find(".prjct").toggle(400);
   });
 
-
+});
